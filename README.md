62b76f64c34816ebd1042cd77f80df547dd5e512
Daniel Lambert <lambo@daniels-mbp.speedport.ip>
2023-10-20 15:28:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# apapche_airflow
---
+++ materials/dags/user_processing.py
@@ -0,0 +1,7 @@
+from airflow import DAG
+from datetime import datetime
+
+with DAG('user_processing', start_date=datetime('2023, 01, 01'),
+        schedule_interval='@daily', catchup=False) as dag:
+    None
+
