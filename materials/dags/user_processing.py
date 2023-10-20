f61c78690adb2d119ae91a49f031bd75430a8b86
Daniel Lambert <lambo@daniels-mbp.speedport.ip>
2023-10-20 15:57:22 +0200

    adding first operator/task

--- materials/dags/user_processing.py
+++ materials/dags/user_processing.py
@@ -1,7 +1,22 @@
 from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime
 
-with DAG('user_processing', start_date=datetime('2023, 01, 01'),
+with DAG('user_processing', start_date=datetime(2023, 1, 1),
         schedule_interval='@daily', catchup=False) as dag:
-    None
+    
+    create_table = PostgresOperator(
+        task_id = 'create_table',
+        postgres_conn_id='postgres',
+        sql= '''
+        CREATE TABLE IF NOT EXISTS users (
+            firstname TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            country TEXT NOT NULL,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL,
+            email TEXT NOT NULL
+        )
+        '''
+    )
 
